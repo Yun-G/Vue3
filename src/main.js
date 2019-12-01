0591cc596f80d7ed74f8681dc7fd5265187b9322
Your Name <1534395864@qq.com>
2019-12-01 18:45:37 +0800

    这个版本没有Vuex

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import http from '@/requset/api'
+
+Vue.use(store) //全局属性和方法
+Vue.use(Element)
+
+Vue.prototype.$http = http; // 将api挂载到vue的原型上复制代码
+
+Vue.config.productionTip = false
+
+new Vue({
+	router,
+	store,
+	render: h => h(App)
+}).$mount('#app')
